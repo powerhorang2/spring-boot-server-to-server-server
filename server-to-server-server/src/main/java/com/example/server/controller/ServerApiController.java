58d39f2b83d4fa1934ba264d38c66a9e952db89b
powerhorang2 <tjrdbqls3@naver.com>
2021-08-19 18:13:03 +0900

    Server - Post 받는 메소드 Controller 에 생성

--- server-to-server-server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server-to-server-server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -1,12 +1,18 @@
 package com.example.server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.server.dto.User;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
@@ -16,6 +22,13 @@ public class ServerApiController {
 		User user = new User();
 		user.setName(name);
 		user.setAge(age);
+		return user;
+	}
+	
+	@PostMapping("/user/{userId}/name/{userName}")
+	public User post(@RequestBody User user, @PathVariable int userId, @PathVariable String userName) {
+		log.info("userId : {}, userName : {}", userId, userName);
+		log.info("clent req : {}", user);
 		return user;
 	}
 }
