e124c423c39127b08f3d9dd74b64dc9a54aabf0b
powerhorang2 <tjrdbqls3@naver.com>
2021-08-20 10:21:21 +0900

    Server - @RequestHeader("{header}") Annotation 으로 Request Header 내용 받기

--- server-to-server-server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server-to-server-server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +27,13 @@ public class ServerApiController {
 	}
 	
 	@PostMapping("/user/{userId}/name/{userName}")
-	public User post(@RequestBody User user, @PathVariable int userId, @PathVariable String userName) {
+	public User post(@RequestBody User user,
+					@PathVariable int userId,
+					@PathVariable String userName,
+					@RequestHeader("x-authorization") String authorization,
+					@RequestHeader("custom-header") String customHeader) {
 		log.info("userId : {}, userName : {}", userId, userName);
+		log.info("authorization : {}, customHeader : {}", authorization, customHeader);
 		log.info("clent req : {}", user);
 		return user;
 	}
